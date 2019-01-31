0015852e6ef3450f4e143285a761a99f82243250
ethankhoa <ethan.huynh@gmail.com>
2019-02-01 09:37:42 +1100

    less stuff

--- src/lib/formatCurrency.js
+++ src/lib/formatCurrency.js
@@ -1,49 +1,12 @@
 import currencyFormatter from 'currency-formatter'
 
-export function formatDynamicAmount(amount, currencyCode) {
+const formatDynamicAmount = (amount, currencyCode) => {
   return currencyFormatter.format(
     amount,
     {code: currencyCode, precision: (amount % 1 ? 2 : 0)}
   )
 }
 
-export function formatPreciseAmount(amount, currencyCode) {
-  return currencyFormatter.format(
-    amount,
-    {code: currencyCode, precision: 2}
-  )
-}
-
-export function formatRoundedAmount(amount, currencyCode) {
-  return currencyFormatter.format(
-    amount,
-    {code: currencyCode, precision: 0}
-  )
-}
-
-export function formatDynamicAmountWithDollarType(amount, currencyCode) {
-  return addDollarType(
-    formatDynamicAmount(amount, currencyCode),
-    currencyCode
-  )
-}
-
-export function formatPreciseAmountWithDollarType(amount, currencyCode) {
-  return addDollarType(
-    formatPreciseAmount(amount, currencyCode),
-    currencyCode
-  )
-}
-
-export function formatRoundedAmountWithDollarType(amount, currencyCode) {
-  return addDollarType(
-    formatRoundedAmount(amount, currencyCode),
-    currencyCode
-  )
-}
-
-function addDollarType(formattedAmount, currencyCode) {
-  return formattedAmount.match(/^\$/)
-    ? `${currencyCode}${formattedAmount}`
-    : formattedAmount
+module.exports = {
+  formatDynamicAmount
 }
--- src/pdf/GiftCard/App.js
+++ src/pdf/GiftCard/App.js
@@ -2,17 +2,7 @@ import React from 'react';
 import Component from './Component';
 
 import { formatDynamicAmount } from '../../lib/formatCurrency'
-
-const fullMonths = [
-  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
-]
-
-const formatDate = (date) => {
-  const monthIdx = date.getMonth()
-  const monthStr = fullMonths[monthIdx]
-
-  return `${date.getDate()} ${monthStr} ${date.getFullYear()}`
-}
+import { formatDate } from '../../lib/formatDate'
 
 export default (props) => {
   const giftCardAmount = formatDynamicAmount(props.gift_card_value, props.currency)

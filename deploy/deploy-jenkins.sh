4b06d9bb85b82a2f318610aadd9e315ab1e138d6
ethankhoa <ethan.huynh@gmail.com>
2019-09-03 17:02:41 +1000

    jenkins

--- deploy/deploy-jenkins.sh
+++ deploy/deploy-jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz --no-clobber
-tar -xvzf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 
 . ~/.nvm/nvm.sh
 nvm install

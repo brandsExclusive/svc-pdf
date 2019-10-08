84df9c92216f7768ad648e5809f2c4b2e204bea8
Sergey Dyachok <sergey@sdyachok.com.ua>
2019-10-08 14:30:07 +1100

    [LEE-76] Redesign the gift card for LE (#8)
    * [LEE-76] Redesign the gift card for LE

--- dist/pdf/GiftCard/Component.js
+++ dist/pdf/GiftCard/Component.js
@@ -14,8 +14,7 @@ var _style2 = _interopRequireDefault(_style);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var logoId = 'https://res.cloudinary.com/lux-group/image/upload/f_auto,fl_progressive,q_auto:eco,c_scale,w_1200/luxuryescapes/luxuryescapes-logo';
-var bowId = 'https://res.cloudinary.com/lux-group/image/upload/f_auto,fl_progressive,q_auto:eco/luxuryescapes/gift-wrap-large';
+var bannerId = 'https://images.luxuryescapes.com/lux-group/image/upload/q_auto:eco,c_scale,w_1200/le-gift-card-banner_lgdnvh';
 
 var redeemInstructions = 'Become a member at LuxuryEscapes.com to choose your perfect escape. At checkout, enter your unique voucher code and your account will be credited with the value of this gift card. The amount will be deducted from the final cost of your chosen holiday. Gift card valid for three years from purchase and must be used by the expiry date.';
 
@@ -33,16 +32,11 @@ var Component = function Component(_ref) {
       _react2.default.createElement(
         'picture',
         { style: _style2.default.picture },
-        _react2.default.createElement('img', { style: _style2.default.bow, src: bowId })
-      ),
-      _react2.default.createElement(
-        'div',
-        { style: _style2.default.section },
-        _react2.default.createElement(
-          'picture',
-          { style: _style2.default.picture },
-          _react2.default.createElement('img', { style: _style2.default.logo, src: logoId })
-        )
+        _react2.default.createElement('img', {
+          style: _style2.default.banner,
+          src: bannerId,
+          alt: 'LuxuryEscapes Gift Card'
+        })
       ),
       _react2.default.createElement(
         'div',
@@ -51,11 +45,6 @@ var Component = function Component(_ref) {
           'div',
           { style: _style2.default.amount },
           amount
-        ),
-        _react2.default.createElement(
-          'div',
-          { style: _style2.default.subHeading },
-          'GIFT VOUCHER'
         )
       ),
       _react2.default.createElement(
@@ -85,7 +74,7 @@ var Component = function Component(_ref) {
         ),
         _react2.default.createElement(
           'div',
-          { style: _style2.default.messsage },
+          { style: _style2.default.message },
           _react2.default.createElement(
             'div',
             { style: _style2.default.leftColumn },
@@ -139,40 +128,68 @@ var Component = function Component(_ref) {
         { style: _style2.default.footer },
         _react2.default.createElement(
           'div',
-          { style: _style2.default.leftColumn },
+          null,
           _react2.default.createElement(
             'div',
-            { style: _style2.default.footerSection },
-            'Voucher Code:'
+            { style: _style2.default.leftColumn },
+            _react2.default.createElement(
+              'div',
+              { style: _style2.default.footerSection },
+              'Voucher Code'
+            )
           ),
           _react2.default.createElement(
             'div',
-            { style: _style2.default.footerText },
-            code
-          ),
-          _react2.default.createElement(
-            'div',
-            { style: _style2.default.footerSection },
-            'Expiry Date:'
-          ),
-          _react2.default.createElement(
-            'div',
-            { style: _style2.default.footerText },
-            expiry
+            { style: _style2.default.rightColumn },
+            _react2.default.createElement(
+              'div',
+              { style: _style2.default.footerText },
+              code
+            )
           )
         ),
         _react2.default.createElement(
           'div',
-          { style: _style2.default.rightColumn },
+          null,
           _react2.default.createElement(
             'div',
-            { style: _style2.default.footerSection },
-            'How To Redeem:'
+            { style: _style2.default.leftColumn },
+            _react2.default.createElement(
+              'div',
+              { style: _style2.default.footerSection },
+              'Expiry Date'
+            )
           ),
           _react2.default.createElement(
             'div',
-            { style: _style2.default.footerText },
-            redeemInstructions
+            { style: _style2.default.rightColumn },
+            _react2.default.createElement(
+              'div',
+              { style: _style2.default.footerText },
+              expiry
+            )
+          )
+        ),
+        _react2.default.createElement(
+          'div',
+          null,
+          _react2.default.createElement(
+            'div',
+            { style: _style2.default.leftColumn },
+            _react2.default.createElement(
+              'div',
+              { style: _style2.default.footerSection },
+              'How To Redeem:'
+            )
+          ),
+          _react2.default.createElement(
+            'div',
+            { style: _style2.default.rightColumn },
+            _react2.default.createElement(
+              'div',
+              { style: _style2.default.footerTextRedeem },
+              redeemInstructions
+            )
           )
         )
       )
